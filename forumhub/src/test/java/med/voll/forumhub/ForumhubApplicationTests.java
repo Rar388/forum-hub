32482463b3fe0d24be64a87765f66c4de22ad0ad
Raul Douglas <rauldouglasoliveira@gmail.com>
2026-03-07 20:07:02 -0300

    primeiro commit

---
+++ forumhub/src/test/java/med/voll/forumhub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
